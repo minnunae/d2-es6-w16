6e09bc9ee837a91cd53bb5aca0091bdebdd051e0
minnunae <69183312+minnunae@users.noreply.github.com>
2021-10-01 10:18:25 +0700

    Add files via upload

---
+++ app102.js
@@ -0,0 +1,7 @@
+
+let message = function myMessage(){
+    document.writeln('My Hello1');
+    return "My hello2";
+}
+
+console.log(message());
---
+++ app104.js
@@ -0,0 +1,8 @@
+(
+    function myHello(){
+        document.writeln("Hello World1!");
+        console.log("Hello World2!");
+    }
+)();
+
+// console.log(myHello());
